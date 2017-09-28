e0aac48f8dbddc422dad054d594b821ed96c674d
Waqar Ahmad <waqar_id@hotmail.com>
2017-09-29 02:48:18 +0500

    Added Properties and  indexer

--- NewFeaturesofDotnetcore/Program.cs
+++ NewFeaturesofDotnetcore/Program.cs
@@ -36,6 +36,90 @@ namespace NewFeaturesofDotnetcore
 
         }
 
+        //Properties
+        /*
+             A property is declared like a field, but with a get/set block added. Here’s how to
+            implement CurrentPrice as a property:
+         */
+
+        public class Stock
+        {
+            //get and set denote property accessors.
+            decimal _currentPrice,sharedPrice;
+            public decimal CurrentPrice
+            {
+                //The get accessor runs when the property is read. It must return a value of the property’s type.
+                get { return _currentPrice; }
+                //The set accessor runs when the  property is assigned. It has an implicit parameter named value
+                set { _currentPrice = value; }
+
+            }
+
+
+            //Read-only and calculated properties
+            //A property is read-only if it specifies only a get accessor,
+
+            public decimal Worth
+            {
+                get { return _currentPrice * sharedPrice; }
+            }
+
+            //Expression-bodied properties
+            public decimal Worth2 => _currentPrice * sharedPrice;
+
+            //Automatic Properties
+            //An automatic property declaration instructs the compiler to provide this implementation.
+            public int Price { get; set; }
+
+            //Property initializers (C# 6)
+            //From C# 6, you can add a property initializer to automatic properties, just as with fields
+            public string Name { get; set; } = "Waqar Ahmad";
+            // This give Name Property and intial value is Waqar Ahmad
+
+            //with an initializer can be  read-only:
+            public string Address { get; } = "Islambad";
+
+            //get and set accessibility
+            //The get and set accessors can have different access levels.
+            //The typical use case for this is to have a public property with an internal or private access modifier on the setter:
+
+            private int _Age;
+            public int Age
+            {
+                get { return _Age; }
+                private set { _Age = value; }
+            }
+
+            /*
+             Notice that you declare the property itself with the more permissive access level
+             (public, in this case) and add the modifier to the accessor you want to be less accessible.
+            */
+
+          
+
+        }
+
+        // Indexers
+        //Indexers are similar to properties but are accessed via an index argument rather than a property name.
+        //The .NET class library design guidelines recommend having only one indexer per class.
+        //So in your case, there's no way to do what you're asking using indexers only.
+
+
+        public class Sentence
+        {
+            string[] words = "The quick brown fox".Split();
+            public string this [int wordNum]
+            {
+                get { return words[wordNum]; }
+                set { words[wordNum] = value; }
+            }
+
+            //public string this[int index] => words [index];
+
+            //public string this[int intx] => words[intx];
+        }
+
+
 
 
         static void Main(string[] args)
@@ -54,7 +138,18 @@ namespace NewFeaturesofDotnetcore
             std.StudentMarks = 565;
             std.StudentName = "Ahmad";
 
-            //Console.WriteLine("Hello World!");
+            // Here’s how we could use this indexer:
+
+            Sentence obj_indexer = new Sentence();
+            Console.WriteLine(obj_indexer[3]);
+            obj_indexer[3] = "NewStringAdded";
+            Console.WriteLine(obj_indexer[3]);
+
+
+
+
+
+            
         }
     }
 }
