54c5f151b4f73f7f8b9816346b849cabfffdf569
Waqar A <Waqar.Ahmad@datum.pk>
2017-09-28 19:19:32 +0500

    Add project files.

---
+++ NewFeaturesofDotnetcore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewFeaturesofDotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
