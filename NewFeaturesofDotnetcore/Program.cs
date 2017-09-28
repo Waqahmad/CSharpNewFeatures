c74a37eec0e4263d5f5b2fc000af37e3d5fc00bb
Waqar Ahmad <waqar_id@hotmail.com>
2017-09-28 19:33:06 +0500

    Test project

--- NewFeaturesofDotnetcore/Program.cs
+++ NewFeaturesofDotnetcore/Program.cs
@@ -4,6 +4,14 @@ namespace NewFeaturesofDotnetcore
 {
     class Program
     {
+
+        //Expression-bodied Methods
+        int foo1 (int x)
+        {
+            return x * 2;
+        }
+        //we can do that like, here foo is expression bodied method
+        int foo (int x) => x * 2;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
