c6f1c8b9d6b675a0eb766e86eb75034b098a95f1
Waqar Ahmad <waqar_id@hotmail.com>
2017-09-29 00:27:22 +0500

    Added new changes , expression bodied method, constructed overloaded, object intializer and optional parameter

--- NewFeaturesofDotnetcore/Program.cs
+++ NewFeaturesofDotnetcore/Program.cs
@@ -12,9 +12,49 @@ namespace NewFeaturesofDotnetcore
         }
         //we can do that like, here foo is expression bodied method
         int foo (int x) => x * 2;
+
+
+        //Overloading Constructor
+        public class Student
+        {
+            public string StudentName;
+            public int StudentMarks;
+            public char Gender;
+            public bool Promoted;
+            public Student() { }
+            //This is called Expression-bodied methods (C# 6)
+            public Student (string name) => StudentName = name;
+            // //This is called Expression-bodied methods (C# 6)
+            public Student(string name, int marks) : this(name) => StudentMarks = marks;
+
+            //Object Initializers Versus Optional Parameters, here G and P are optional Parameter
+            public Student(string name, int marks, char G='M', bool p=false) : this(name, marks)
+            {
+                Gender = G;
+                Promoted = p;
+            }
+
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //// Note parameterless constructors can omit empty parentheses
+            Student _stud = new Student { StudentName = "Waqar", StudentMarks = 585 };
+
+            //Using object initializers, you can instantiate Bunny objects as follows:
+            Student _std2 = new Student("Waqar") { StudentMarks = 45 };
+
+            //Object Initializers Versus Optional Parameters
+            Student _std3 = new Student("Waqar", 32, p: true);
+
+
+            Student std = new Student();
+            std.StudentMarks = 565;
+            std.StudentName = "Ahmad";
+
+            //Console.WriteLine("Hello World!");
         }
     }
 }
